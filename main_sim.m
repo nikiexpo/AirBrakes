4deb6e24fc1eb68ca4ea71e8c7eb765598125ab5
nikilesh <nikileshramesh2003@gmail.com>
2023-07-02 00:56:50 +0100

    debug attempt #1

--- main_sim.m
+++ main_sim.m
@@ -7,3 +7,12 @@ clear; clc;
 x = rocketDynamics;
 disp(x.massAtBurnout)
 disp(x.state')
+
+n = 1000;
+dt = 0.1;
+Vel = zeros(n,3);
+
+for i = 1:10
+    x = x.update(dt);
+    Vel(i,:) = x.linearVelocity';
+end
--- rocketDynamics.m
+++ rocketDynamics.m
@@ -47,8 +47,8 @@ classdef rocketDynamics
                 obj.nomAirBrakeDragCoeff = 1.17;
                 obj.airBrakePosition = 1.54;
                 obj.maxAirBrakeLength = 0.021;
-                obj.dragCoeffABon = 0.35;
-                obj.dragCoeffABoff = 0.79;
+                obj.dragCoeffABon = 0.79;
+                obj.dragCoeffABoff = 0.35;
                 obj.normalDragCoeff = 13.6;
                 obj.dampDragCoeff = 4.88;
                 obj.stabilityMargin = 0.408;
@@ -57,20 +57,24 @@ classdef rocketDynamics
                 obj.windVeloctiy = [10, 0 , 0]'; %constant wind for now (its a function of alt) -- see eq 27
 
                 %initialize the state
-                obj.position = [0, 0, 0]';
+                obj.position = [0, 0, 232]';
                 obj.quaternion = [1, 0, 0, 0]';
-                obj.linearVelocity = [0, 0, 0]';
-                obj.angularVelocity = [0, 0, 0]';
+                obj.linearVelocity = [0, 0, 166]';
+                obj.angularVelocity = [0.3, 0, 2]';
                 obj.state = [obj.position', obj.quaternion', obj.linearVelocity', obj.angularVelocity']';
                 obj.positionOfCoP = [0, 0, 0.2]'; %depends on OR sim or maybe find a way to calculate CoP
                 obj.rollAxis = [0 0 1]; % assuming this for now, not sure, double check
             end
         end
         
-%         function [new_state] = update(dt)
-% 
-%         end
-% 
+        function [obj] = update(obj,dt)
+            obj.position = obj.position + r_dot(obj).*dt;
+            obj.quaternion = quatmultiply(obj.quaternion', q_dot(obj)');
+            obj.quaternion = obj.quaternion ./ norm(obj.quaternion);
+            obj.linearVelocity = obj.linearVelocity + v_dot(obj).*dt; % VELOCITY DOESNT CHANGE
+            obj.angularVelocity = obj.angularVelocity + omega_dot(obj).*dt;
+        end
+
 %         function [r_k] = positionUpdate(obj,dt)
 %             r_k = obj.position + obj.velocity.*dt;
 %         end
@@ -87,14 +91,16 @@ classdef rocketDynamics
             q_w = obj.quaternion(1);
             q_v = obj.quaternion(2:end);
             omega = obj.angularVelocity;
-            q = [0.5.*(dot(omega, q_v)), 0.5.*(q_w.*omega + cross(omega, q_v))];
+            imag_part = q_w.*omega + cross(omega', q_v')';
+            scalar_part = 0.5.*(dot(omega, q_v));
+            q = [scalar_part, 0.5.*(imag_part)']';
         end
         function [v] = v_dot(obj)
             g = [0 0 -9.81]'; % wrong, gravity vector changes over time https://forum.arduino.cc/t/3d-maths-to-sutract-gravity-from-accelerometer-data/258105/10
             F_g = obj.massAtBurnout.*g; % this also should be a vector, need to compute the gravity vector
             
             [~, a, ~, rho] = atmosisa(obj.position(3)); %density and speed of sound from isa model
-            C_A_rocket = obj.dragCoeffABon / sqrt(1 - (obj.linearVelocity(end)/a)^2 ); %compressibility compensated axial drag coeff
+            C_A_rocket = obj.dragCoeffABon / sqrt(1 - (norm(obj.linearVelocity)/a)^2 ); %compressibility compensated axial drag coeff
             
             % CALCULATION AVOIDED FOR NOW
             %delta = 0.7; % BOUNDARY LAYER THICKNESS :: NEEDS TO BE CALCULATED; PLACEHOLDER FOR NOW  
@@ -105,26 +111,31 @@ classdef rocketDynamics
 
             e_roll = obj.rollAxis; % assuming this for now, not sure, double check
 
-            V_CoP = obj.linearVelocity + cross(obj.angularVelocity, (obj.positionOfCoP - obj.position))';
+            V_CoP = obj.linearVelocity + cross(obj.angularVelocity', (obj.positionOfCoP - obj.position)')';
             V_app = V_CoP + obj.windVeloctiy;
             obj.apparentVelocity = V_app;
             norm_V_app = V_app / norm(V_app);
             F_A = ((-1*rho/2) * C_A_total * obj.referenceArea * norm(V_app)^2).*e_roll; % this should be a vector but dont know how to compute e_roll yet eq 24b
             
-            AoA = acosd(dot(norm_V_app, e_roll));
+            AoA = acos(dot(norm_V_app, e_roll)); % SHOULD THIS BE RADIANS or degs???
             C_N = obj.normalDragCoeff*AoA;
-            F_N = ((-1*rho/2) * C_N * obj.referenceArea * norm(V_app)^2).*(cross(e_roll, cross(e_roll, norm_V_app)));
+            F_N = ((-1*rho/2) * C_N * obj.referenceArea * norm(V_app)^2).*(cross(e_roll', cross(e_roll', norm_V_app')))';
             obj.normalForce = F_N;
             v = (1/obj.massAtBurnout).*(F_g + F_N + F_A);
         end
         function omega = omega_dot(obj)
-            torque_N = (obj.stabilityMargin * norm(obj.normalForce)).*cross(obj.rollAxis, obj.apparentVelocity);
+            torque_N = (obj.stabilityMargin * norm(obj.normalForce)).*cross(obj.rollAxis', obj.apparentVelocity')';
     
             %Constructing the rotation matrix 
-            R = quat2rotm(obj.quaternion);
-
+            R = quat2rotm(obj.quaternion'); % THIS IS NAN SEE WHY? -- not NAN after changing the omega intial conditions
+            I = obj.diagonalInertia;
             damping_torque = (-obj.dampDragCoeff).*((R*diag([1 1 0])/R)*obj.angularVelocity);
             omega = I\(torque_N + damping_torque);
         end
     end
 end 
+
+%% TODo
+% The whole thing is unstable -- suspect the integration method, do RK4
+% impelement the remaining dynamics -- not the variations just the
+% deterministic ones
